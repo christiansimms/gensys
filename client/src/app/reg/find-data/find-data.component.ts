5d1468c61f1c5dfe4c8d2a5b7518e21fef0e2464
Christian Simms <csimms@botonomy.com>
2021-06-06 21:29:01 -0400

    Show data.

--- client/src/app/reg/find-data/find-data.component.ts
+++ client/src/app/reg/find-data/find-data.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs';
+import {ActivatedRoute, Params} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-find-data',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./find-data.component.scss']
 })
 export class FindDataComponent implements OnInit {
+  dataOb: Observable<any>;
 
-  constructor() { }
+  constructor(
+    public route: ActivatedRoute,
+    private http: HttpClient,
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      const id = params.id;
+      this.dataOb = this.http.get<any[]>(`/api/data/entity/${id}?includeParent=true`);
+    });
   }
-
 }
