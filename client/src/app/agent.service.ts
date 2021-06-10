2eb5b226ae04658c98f74e9c7483c421a2e3fec8
Christian Simms <csimms@botonomy.com>
2021-06-09 21:22:33 -0400

    Loop for movement.

--- client/src/app/agent.service.ts
+++ client/src/app/agent.service.ts
@@ -6,10 +6,14 @@ export interface Position {
   z: number;
 }
 
+let UNIQUE_ID = 0;
+
 abstract class Agent {
+  uniqueId: number;
   position: Position;
   constructor(public agentService: AgentService) {
     // this.position = {x: 0, y: 0, z: 0};
+    this.uniqueId = ++UNIQUE_ID;
   }
 
   getValue(xOffset: number, yOffset: number): any {
@@ -26,7 +30,7 @@ class BlobAgent extends Agent {
     }
 
     // Move down diagonally.
-    this.agentService.moveAgent(this, this.position.x + 1, this.position.y + 1, this.position.z + 1);
+    this.agentService.moveAgent(this, this.position.x + 1, this.position.y + 1, this.position.z);
   }
 
   toString(): string {
@@ -51,6 +55,7 @@ function makeAgent(name: string, agentService: AgentService): Agent {
   providedIn: 'root'
 })
 export class AgentService {
+  agentIndex: {[key: number]: Agent} = {};
   dataLayers: any[] = [];
 
   constructor() {
@@ -65,16 +70,27 @@ export class AgentService {
   }
 
   run(name: string): void {
-    const agent = makeAgent(name, this);
-    this.moveAgent(agent, 0, 0, 1);
+    {
+      const agent = makeAgent(name, this);
+      this.agentIndex[agent.uniqueId] = agent;
+      this.moveAgent(agent, 0, 0, 1);
+    }
+
+    // Run a few times.
+    setInterval(() => {
+      console.log('Running step');
+      Object.values(this.agentIndex).forEach(agent => {
+        agent.doStep();
+      });
+    }, 500);
   }
 
   // Update 2 things when moving: agent's position, plus their spreadsheet location.
   moveAgent(agent: Agent, x: number, y: number, z: number): void {
     // Remove from spreadsheet.
     if (agent.position) {
-      const oldCell = this.dataLayers[agent.position.z][agent.position.y][agent.position.x];
-      console.log('OLD CELL:', oldCell);
+      this.dataLayers[agent.position.z][agent.position.y][agent.position.x] = null;
+      // console.log('OLD CELL:', oldCell);
     }
 
     // Update agent.
